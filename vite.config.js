99184bc90ce745f9d985c2b890c4991fd0b2e6cb
khizarusestate <khizarusestate@gmail.com>
2026-04-20 19:47:58 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'framer-motion': ['framer-motion']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['framer-motion']
+  }
 })
